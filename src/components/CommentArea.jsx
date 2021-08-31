be93db1b0c2f2381b8b6fdcd5d77cd7fb6b9c0a5
edwardTunkl <eddytunkl@googlemail.com>
2021-08-31 15:57:54 +0200

    commentArea transformed

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,53 +1,60 @@
-import { Component } from 'react'
+import { useState, useEffect } from 'react'
 import CommentList from './CommentList'
 import AddComment from './AddComment'
 import Loading from './Loading'
 import Error from './Error'
 
-class CommentArea extends Component {
+const CommentArea = (asin) => {
 
-    state = {
-        comments: [], // comments will go here
-        isLoading: false,
-        isError: false
-    }
+    // state = {
+    //     comments: [], // comments will go here
+    //     isLoading: false,
+    //     isError: false
+    // }
+    const [comments, setComments] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [isError, setIsError] = useState(false)
 
-    componentDidUpdate = async (prevProps) => {
-        if (prevProps.asin !== this.props.asin) {
-            this.setState({
-                isLoading: true
-            })
-            try {
-                let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + this.props.asin, {
-                    headers: {
-                        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjViYjJkNTI2MjAwMTViNmRjOTMiLCJpYXQiOjE2MjkyODc4NjcsImV4cCI6MTYzMDQ5NzQ2N30.HkhDkrIcH7q04AsuHParGAbLEKxc3bvsAnjh3DGfZIE"
-                    }
-                })
-                console.log(response)
-                if (response.ok) {
-                    let comments = await response.json()
-                    this.setState({ comments: comments, isLoading: false, isError: false })
-                } else {
-                    console.log('error')
-                    this.setState({ isLoading: false, isError: true })
+    useEffect(() => {
+
+        const fetchFunction = async (prevProps) => {
+            if (prevProps.asin !== asin) {
+                setIsLoading(true)
                 }
-            } catch (error) {
-                console.log(error)
-                this.setState({ isLoading: false, isError: true })
+                try {
+                    let response = await fetch('https://striveschool-api.herokuapp.com/api/comments/' + asin, {
+                        headers: {
+                            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFjZjViYjJkNTI2MjAwMTViNmRjOTMiLCJpYXQiOjE2MjkyODc4NjcsImV4cCI6MTYzMDQ5NzQ2N30.HkhDkrIcH7q04AsuHParGAbLEKxc3bvsAnjh3DGfZIE"
+                        }
+                    })
+                    console.log(response)
+                    if (response.ok) {
+                        let comments = await response.json()
+                       setComments(comments)
+                       setIsLoading(false)
+                       setIsError(false)
+                    } else {
+                        console.log('error')
+                        setIsLoading(false)
+                        setIsError(true)
+                    }
+                } catch (error) {
+                    console.log(error)
+                    setIsLoading(false)
+                    setIsError(true)
+                }
             }
-        }
-    }
+        fetchFunction()
+            
+        },[asin]) 
 
-    render() {
         return (
             <div>
-                {this.state.isLoading && <Loading />}
-                {this.state.isError && <Error />}
-                <AddComment asin={this.props.asin} />
-                <CommentList commentsToShow={this.state.comments} />
+                {isLoading && <Loading />}
+                {isError && <Error />}
+                <AddComment asin={asin} />
+                <CommentList commentsToShow={comments} />
             </div>
         )
-    }
 }
-
 export default CommentArea
